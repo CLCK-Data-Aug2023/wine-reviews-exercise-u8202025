cb3ecbbf312c556b6e3e7cace4830f8b43992f85
u8202025 <u8202025@gmail.com>
2023-12-24 23:48:50 -0500

    homework

--- reviews.py
+++ reviews.py
@@ -1,2 +1,11 @@
 # add your code here
 
+import pandas as pd
+reviews = pd.read_csv("data/winemag-data-130k-v2.csv", index_col=0)
+
+counts = reviews['country'].value_counts()
+avg = reviews.groupby('country')['points'].mean().round(1)
+combine = pd.DataFrame({'count': counts, 'points': avg})
+table = combine.sort_values(by='count', ascending=False)
+table.index.name = 'country'
+table.reset_index().to_csv("reviews-per-country.csv", index=False)
